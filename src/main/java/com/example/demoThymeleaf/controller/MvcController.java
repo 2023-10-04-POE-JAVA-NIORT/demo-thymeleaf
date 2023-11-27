c5fee9c9e568180dba4f9e9f214fa53ae689deb8
jcdominguez <jcd.pro@gmail.com>
2023-11-27 14:09:35 +0100

    exemples thymeleaf POST

--- src/main/java/com/example/demoThymeleaf/controller/MvcController.java
+++ src/main/java/com/example/demoThymeleaf/controller/MvcController.java
@@ -4,6 +4,7 @@ import com.example.demoThymeleaf.metier.Personne;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -11,6 +12,8 @@ import java.util.List;
 
 @Controller
 public class MvcController {
+
+    List<Personne> personnes = new ArrayList<>();
 
     @GetMapping("hello")
     public String getHello(Model model, @RequestParam String prenom){
@@ -33,13 +36,22 @@ public class MvcController {
     @GetMapping("personnes")
     public String getPersonnes(Model model){
 
-        List<Personne> personnes = new ArrayList<>();
+
         //personnes.add(new Personne("Marie", "Dupont", 16));
         //personnes.add(new Personne("Alain", "Delon", 16));
         //personnes.add(new Personne("JC", "Dominguez", 16));
 
         model.addAttribute("personnes", personnes);
 
+        return "personnes.html";
+    }
+
+    @PostMapping("personnes")
+    public String addPersonne(Personne personne, Model model){
+
+        personnes.add(personne);
+
+        model.addAttribute("personnes", personnes);
         return "personnes.html";
     }
 }
